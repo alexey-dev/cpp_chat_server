593ae4a08683cf1688114058eff1cc539b83292e
Alexey Usachov <usachov.alexey.dev@gmail.com>
2025-11-06 21:42:43 +0100

    Added test for ServerArgumentsParser

--- tests/ServerArgumentsParserTests.cpp
+++ tests/ServerArgumentsParserTests.cpp
@@ -36,4 +36,69 @@ TEST(ServerArgumentsParserTest, Parse_PassPortWithNumericValue_ReturnsTrue) {
   EXPECT_TRUE(Parser.Parse(Argc, Argv));
 }
 
+TEST(ServerArgumentsParserTest, Parse_PortMissing_ReturnsFalse) {
+  ServerArgumentsParser Parser;
+  char      Argv0[] = "path/to/executable";
+  char *    Argv[]  = {Argv0};
+  const int Argc    = sizeof(Argv)/sizeof(char*);
+
+  EXPECT_FALSE(Parser.Parse(Argc, Argv));
+}
+
+TEST(ServerArgumentsParserTest, Parse_PassPortSSLAndCertificatePathAndPrivateKeyPathMissing_ReturnsFalse) {
+  ServerArgumentsParser Parser;
+  char      Argv0[] = "path/to/executable";
+  char      Argv1[] = "--port";
+  char      Argv2[] = "10001";
+  char      Argv3[] = "--ssl";
+  char *    Argv[]  = {Argv0, Argv1, Argv2, Argv3};
+  const int Argc    = sizeof(Argv)/sizeof(char*);
+
+  EXPECT_FALSE(Parser.Parse(Argc, Argv));
+}
+
+TEST(ServerArgumentsParserTest, Parse_PassPortSSLAndPrivateKeyPathMissing_ReturnsFalse) {
+  ServerArgumentsParser Parser;
+  char      Argv0[] = "path/to/executable";
+  char      Argv1[] = "--port";
+  char      Argv2[] = "10001";
+  char      Argv3[] = "--ssl";
+  char      Argv4[] = "--cert";
+  char      Argv5[] = "sslkeys/selfsigned.key";
+  char *    Argv[]  = {Argv0, Argv1, Argv2, Argv3, Argv4, Argv5};
+  const int Argc    = sizeof(Argv)/sizeof(char*);
+
+  EXPECT_FALSE(Parser.Parse(Argc, Argv));
+}
+
+TEST(ServerArgumentsParserTest, Parse_PassPortSSLAndCertificatePathMissing_ReturnsFalse) {
+  ServerArgumentsParser Parser;
+  char      Argv0[] = "path/to/executable";
+  char      Argv1[] = "--port";
+  char      Argv2[] = "10001";
+  char      Argv3[] = "--ssl";
+  char      Argv4[] = "--key";
+  char      Argv5[] = "sslkeys/private.key";
+  char *    Argv[]  = {Argv0, Argv1, Argv2, Argv3, Argv4, Argv5};
+  const int Argc    = sizeof(Argv)/sizeof(char*);
+
+  EXPECT_FALSE(Parser.Parse(Argc, Argv));
+}
+
+TEST(ServerArgumentsParserTest, Parse_PassPortSSLCertificatePathPrivateKeyPath_ReturnsTrue) {
+  ServerArgumentsParser Parser;
+  char      Argv0[] = "path/to/executable";
+  char      Argv1[] = "--port";
+  char      Argv2[] = "10001";
+  char      Argv3[] = "--ssl";
+  char      Argv4[] = "--key";
+  char      Argv5[] = "sslkeys/private.key";
+  char      Argv6[] = "--cert";
+  char      Argv7[] = "sslkeys/selfsigned.key";
+  char *    Argv[]  = {Argv0, Argv1, Argv2, Argv3, Argv4, Argv5, Argv6, Argv7};
+  const int Argc    = sizeof(Argv)/sizeof(char*);
+
+  EXPECT_TRUE(Parser.Parse(Argc, Argv));
+}
+
 }  // namespace
